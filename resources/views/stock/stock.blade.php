8d8a91925435739d9c0a736d640a709a7bf1682f
Zero <matiasgr97@Outlook.com>
2018-08-10 00:06:56 -0300

    v1.3

--- resources/views/stock/stock.blade.php
+++ resources/views/stock/stock.blade.php
@@ -161,7 +161,7 @@ $(document).ready(function(){
                     onClose: function(selectedDate) {
                     },
                     onSelect: function(dateText, inst) {
-                        $('#fecha').attr('value',dateText);
+                        $('#fecha_1').attr('value',dateText);
                     }
                 }).datepicker("setDate", "0");
     $('#codbarras_1').select2({
@@ -240,7 +240,7 @@ function addRow(){
                     onClose: function(selectedDate) {
                     },
                     onSelect: function(dateText, inst) {
-                        $('#fecha').attr('value',dateText);
+                        $('#fecha_'+i).attr('value',dateText);
                     }
                 }).datepicker("setDate", "0");
     }
@@ -355,7 +355,30 @@ $(document).on('click', '.btn_remove', function(){
     $('#row'+button_id).remove();
 }); 
 
-
+jQuery(function($) {
+    $.datepicker.regional['es'] = {
+        closeText: 'Cerrar',
+        prevText: '&#x3c;Ant',
+        nextText: 'Sig&#x3e;',
+        currentText: 'Hoy',
+        monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+            'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
+        ],
+        monthNamesShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun',
+            'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'
+        ],
+        dayNames: ['Domingo', 'Lunes', 'Martes', 'Mi&eacute;rcoles', 'Jueves', 'Viernes', 'S&aacute;bado'],
+        dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mi&eacute;', 'Juv', 'Vie', 'S&aacute;b'],
+        dayNamesMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'S&aacute;'],
+        weekHeader: 'Sm',
+        dateFormat: 'yy/mm/dd',
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ''
+    };
+    $.datepicker.setDefaults($.datepicker.regional['es']);
+});
 </script>
 
 @endsection
